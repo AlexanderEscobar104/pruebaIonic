61eb35d4c6508e1f213422c814a0c457e311b9a9
Alexander Mauricio Escobar Arbelaez <alex.escobar104@gmail.com>
2026-07-18 13:00:03 -0500

    chore: configure Firebase credentials for pruebaionic project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_PROJECT.firebaseapp.com',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT.appspot.com',
-    messagingSenderId: 'YOUR_SENDER_ID',
-    appId: 'YOUR_APP_ID',
+    apiKey: 'AIzaSyCwikskavz8Bk2v_bd6jetEldgeTnCo09M',
+    authDomain: 'pruebaionic-191ab.firebaseapp.com',
+    projectId: 'pruebaionic-191ab',
+    storageBucket: 'pruebaionic-191ab.firebasestorage.app',
+    messagingSenderId: '251802121754',
+    appId: '1:251802121754:web:c259b7493fea3bde13c3a3',
+    measurementId: 'G-04WV0CZR7N',
   },
 };
